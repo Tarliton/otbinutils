27592c7002ffad7e3cb9a080352ddca02a8f20ad
Tarliton Godoy <tarlitongodoy@gmail.com>
2016-02-15 16:12:26 -0200

    update sprite

--- otbinutils/tspr/tspr.py
+++ otbinutils/tspr/tspr.py
@@ -1,4 +1,5 @@
 from otbinutils.fileutils import fileutils
+import json
 
 SPRITE_SIZE = 32
 SPRITE_DATA_SIZE = SPRITE_SIZE * SPRITE_SIZE * 4
@@ -10,10 +11,18 @@ class TSpr():
         self.file = fileutils.File(version, "spr")
         self.__read_file()
 
+    def __iter__(self):
+        for key, value in list(self.__dict__.items()):
+            if key == "file":
+                continue
+            else:
+                yield (key, value)
+
     def __read_file(self):
         self.spr_version = self.file.read_int32()
         self.spr_count = self.file.read_int32()
         sprite_offset = self.file.tell()
+        self.sprites = []
 
         for sprite_id in range(self.spr_count):
             self.file.seek(sprite_id * 4 + sprite_offset)
@@ -36,17 +45,23 @@ class TSpr():
                 transparent_pixels = self.file.read_int16()
                 colored_pixels = self.file.read_int16()
 
-                for pixel in range(transparent_pixels) (if write_pos < SPRITE_DATA_SIZE):
+                pixel = 0
+                while pixel < transparent_pixels and write_pos < SPRITE_DATA_SIZE:
+                    pixel = pixel + 1
                     write_pos = write_pos + 4
 
-                for pixel in range(colored_pixels) (if write_pos < SPRITE_DATA_SIZE):
+                pixel = 0
+                while pixel < colored_pixels and write_pos < SPRITE_DATA_SIZE:                   
                     pixels[write_pos + 0] = self.file.read_int16()
                     pixels[write_pos + 1] = self.file.read_int16()
                     pixels[write_pos + 2] = self.file.read_int16()
+                    pixel = pixel + 1
                     write_pos = write_pos + 4
+
 
                 read = read + 3 * colored_pixels
 
+            self.sprites.append(pixels)
 
 
 
@@ -54,3 +69,7 @@ class TSpr():
         print("count:", self.spr_count)
 
         print("ae")
+
+    def to_json(self):
+        with open("output/"+str(self.version)+"/tspr.json", 'w+') as outfile:
+            json.dump(self, outfile, default=lambda o: dict(o), sort_keys=True, indent=4, ensure_ascii=False)
